87545ef444d7781d900bb0d3e103ebbfe029a229
Mateus Santana Rios <mateus.rios@MJV.net>
2022-04-12 20:01:16 -0300

    Inclusao mutiny

--- src/main/java/com/github/msrios/GreetingResource.java
+++ src/main/java/com/github/msrios/GreetingResource.java
@@ -1,5 +1,8 @@
 package com.github.msrios;
 
+import io.smallrye.mutiny.Uni;
+import org.jboss.resteasy.reactive.RestPath;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -12,5 +15,20 @@ public class GreetingResource {
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
         return "Hello RESTEasy";
+    }
+
+    @RestPath
+    String texto;
+
+    @GET
+    @Path("mutiny/{texto}")
+    @Produces(MediaType.TEXT_PLAIN)
+    public Uni<String> mutiny() {
+        return Uni.createFrom().item(texto).onItem().invoke(item -> {
+            if ("error".equals(texto)) throw new IllegalArgumentException();
+        }).onFailure().recoverWithItem(f -> {
+            System.out.println(f);
+            return "ERRINHO";
+        }).map(String::toUpperCase);
     }
 }
